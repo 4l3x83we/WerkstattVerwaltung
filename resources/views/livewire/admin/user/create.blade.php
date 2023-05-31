6497094fc130109eb605d7a2a3edb223a3c8e7cf
Alexander Guthmann <4l3x83we.dev@gmail.com>
2023-05-31 08:21:50 +0200

    change view

--- resources/views/livewire/admin/user/create.blade.php
+++ resources/views/livewire/admin/user/create.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <div class="p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-        <div class="w-full mb-1">
+    <div class="grid grid-cols-1 p-4 xl:grid-cols-3 xl:gap-4 dark:bg-gray-900">
+        <div class="mb-4 col-span-full xl:mb-2">
             <div class="breadcrumbs mb-4">
                 {!! Breadcrumbs::render('benutzerCreate') !!}
                 <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">Benutzer erstellen</h1>
@@ -22,9 +22,7 @@
                 </div>
             </div>
         </div>
-    </div>
-    <x-ag.main.head>
-        <div class="p-4">
+        <div class="col-span-full">
             <x-ag.card.head>
                 <h3 class="mb-4 text-xl font-semibold dark:text-white">{{ __('Create new user') }}</h3>
                 <form wire:submit.prevent="create">
@@ -60,5 +58,5 @@
                 </form>
             </x-ag.card.head>
         </div>
-    </x-ag.main.head>
+    </div>
 </div>
