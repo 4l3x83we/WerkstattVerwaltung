2a0542ad059d337a05560b5c1093fc078c56b3c1
Alexander Guthmann <4l3x83we.dev@gmail.com>
2023-06-14 06:10:14 +0200

    add Vehicles

--- resources/views/livewire/backend/vehicles/vehicle-create.blade.php
+++ resources/views/livewire/backend/vehicles/vehicle-create.blade.php
@@ -1,3 +1,14 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <div class="grid grid-cols-1 p-4 pb-0 xl:grid-cols-3 xl:gap-4 dark:bg-gray-900">
+        <div class="mb-4 col-span-full xl:mb-2">
+            <div class="breadcrumbs">
+                {!! Breadcrumbs::render('vehiclesCreate') !!}
+                <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">Neues Fahrzeug anlegen</h1>
+                <x-ag.errors.errorMessages />
+            </div>
+        </div>
+    </div>
+    <x-ag.main.head>
+
+    </x-ag.main.head>
 </div>
--- resources/views/livewire/backend/vehicles/vehicle-edit.blade.php
+++ resources/views/livewire/backend/vehicles/vehicle-edit.blade.php
@@ -1,3 +1,14 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <div class="grid grid-cols-1 p-4 pb-0 xl:grid-cols-3 xl:gap-4 dark:bg-gray-900">
+        <div class="mb-4 col-span-full xl:mb-2">
+            <div class="breadcrumbs">
+                {!! Breadcrumbs::render('vehiclesEdit', $fahrzeuge) !!}
+                <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">Fahrzeug bearbeiten: {{ $fahrzeuge->vehicles_license_plate }}</h1>
+                <x-ag.errors.errorMessages />
+            </div>
+        </div>
+    </div>
+    <x-ag.main.head>
+
+    </x-ag.main.head>
 </div>
