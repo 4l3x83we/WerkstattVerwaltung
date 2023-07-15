9ddf506eb83e7177eb99b40b06d26126d4ca0a65
Alexander Guthmann <4l3x83we.dev@gmail.com>
2023-07-15 20:29:47 +0200

    add Livewire Charts

---
+++ app/Http/Livewire/LivewireCharts.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LivewireCharts extends Component
+{
+    public function render()
+    {
+        return view('livewire.livewire-charts');
+    }
+}
